42769ba6c01b8e529e989e41c68a58f37e87edf0
Albert Tomanek <electron826@gmail.com>
2016-05-25 19:56:22 +0100

    c1

--- spam.c
+++ spam.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define STRLEN 256
 #define VERSION 1	// int 1 - 255
+#define SPAMFILESIZE 65534
+#define SPAMHEADERSIZE 63
 
 int main()
 {
@@ -22,6 +25,11 @@ int main()
 		fputc('M', out);
 	
 		fputc(VERSION, out);
+	
+		fputc(0, out);
+		fputc(0, out);
+	
+		fwrite( &(uint16_t) (SPAMFILESIZE - SPAMHEADERSIZE), sizeof(uint16_t), 1, out); 		// fwrite( *data, sizeof data, count, filestream);
 
 		fclose(out);
 		
