d40bbe62c43df3ce445c64589f820e665f5f0cb6
Albert Tomanek <electron826@gmail.com>
2016-05-31 14:42:45 +0100

    c6

--- spam.c
+++ spam.c
@@ -11,10 +11,12 @@
 #define COMMAND "./spam"        // This is the string put into the 'command' field, which can be executed. Note that it is limited to 30 chars long.
 
 // define the shell-specific commands...
+//#define COMMAND_CLEAR "del *.SPA"
 #define COMMAND_CLEAR "rm *.SPA"
 
 // For the .SPA spamfile format, see "spam format.asc"
 
+void explain();
 void generaterandomdata( uint8_t *data, int count);
 uint32_t sdbm_hash(uint8_t *data, int length);          // Hashes an array of bits
 void fputstrn(char *chars, FILE *file, int count);      // like fputs, but specifies limit
@@ -29,6 +31,17 @@ int main(int argc, char *argv[])
 	else if (! strcmp(argv[1], "unspam"))
 	{
 		system(COMMAND_CLEAR);
+	}
+	else if (! strcmp(argv[1], "explain"))
+	{
+		explain();
+	}
+	else if (! strcmp(argv[1], "help")   ||
+			 ! strcmp(argv[1], "-h")     ||
+			 ! strcmp(argv[1], "--help") ||
+			 ! strcmp(argv[1], "/?")     )
+	{
+		printf("%s [unspam, explain, help]\n", __FILE__);
 	}
 	
 	return 0;
@@ -67,8 +80,8 @@ void spam()
 	
 		fputc((uint8_t) VERSION, out);   // format version
 	
-		fputc(0x00, out);               // padding
-		fputc(0x00, out);
+		fputc(0x1a, out);               // DOS EOF character
+		fputc(0x00, out);				// padding
 		
 		fwrite( &datasize , sizeof(uint16_t), 1, out);          // fwrite( *data, sizeof data, count, filestream);
 		
@@ -111,6 +124,40 @@ void spam()
 		
 	}
 }
+
+void explain()
+{
+	FILE *out;
+	out = fopen("SPAM.TXT", "w");
+	
+	const char *string = "\
+ FORMAT OF SPAM FILES                                                     \n\
+ ====================                                                     \n\
+                                                                          \n\
+ Byte              Use                         Value                      \n\
+                                                                          \n\
+ 0,1,2,3           Header                      53, 50, 41, 4D (\"SPAM\")  \n\
+ 4                 Format version              8-bit int                  \n\
+ 5,6               padding                     1A, 00                     \n\
+ 7,8               length of data              16-bit int                 \n\
+ 9,10              padding                     00, 00                     \n\
+ 11,12,13,14       File ID                     32-bit int                 \n\
+ 15,16             padding                     00, 00                     \n\
+ 17 - 25           sdbm hash of random data    32-bit int                 \n\
+ 26,27             padding                     00, 00                     \n\
+ 28,29             private use                 FF, FF                     \n\
+ 30                padding                     00                         \n\
+ 31 - 61           command to execute          null term. string          \n\
+ 63                padding                     00                         \n";
+	
+	fputs(string, out);
+	fclose(out);
+	
+	printf("See SPAM.TXT\n");
+}
+
+
+/****************************/
 
 void generaterandomdata( uint8_t *data, int count)
 {
